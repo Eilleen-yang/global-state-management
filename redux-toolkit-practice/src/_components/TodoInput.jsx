4ea9be31c65e3db603f91d7306766d3784d89199
양아름 <yangareum0130@gmail.com>
2025-05-07 10:30:19 +0900

    feat : 화면구현 dispatch불러오기

--- redux-toolkit-practice/src/_components/TodoInput.jsx
+++ redux-toolkit-practice/src/_components/TodoInput.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import { addTodo } from "../features/todos/todosSlice";
+import { useDispatch } from "react-redux";
+
 export default function TodoInput() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const handleAdd = () => {
+    if (text.trim() === "") return;
+    dispatch(addTodo(text));
+    setText("");
+  };
   return (
     <div>
-      <input type="text" />
-      <button>추가</button>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleAdd}>추가</button>
     </div>
   );
 }
--- redux-toolkit-practice/src/_components/TodoList.jsx
+++ redux-toolkit-practice/src/_components/TodoList.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTodos, removeTodo } from "../features/todos/todosSlice";
+
 export default function TodoList() {
+  const { todos, isLoading, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, [dispatch]);
+
+  if (isLoading) return <p>...로딩 중...</p>;
+  if (error) return <p>error .. {error}</p>;
   return (
     <ul>
-      <li>
-        <button>삭제</button>
-      </li>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.title}</span>
+          <button onClick={() => dispatch(removeTodo(todo.id))}>삭제</button>
+        </li>
+      ))}
     </ul>
   );
 }
