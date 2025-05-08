85f6583704667fb98a6ac682201277b848ca304c
양아름 <yangareum0130@gmail.com>
2025-05-08 09:40:20 +0900

    비동기, actions 추가

--- zustand/src/_compontntes/TodoList.jsx
+++ zustand/src/_compontntes/TodoList.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { useTodoStore } from "../store/todoStore";
 
 export default function TodoList() {
   const todos = useTodoStore((state) => state.todos);
-  const removeTodo = useTodoStore((state) => state.removeTodo);
+  const isLoading = useTodoStore((state) => state.isLoading);
+  const error = useTodoStore((state) => state.error);
 
+  const { fetchTodos, removeTodo } = useTodoStore((state) => state.actions);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  if (isLoading) return <p>...Loading.</p>;
+  if (error) return <p>에러 발생 : {error} </p>;
   return (
     <ul>
       {todos.map((todo) => (
