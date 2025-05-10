9f7e8de2ce17237272b306929bb319db2a3ae7c4
양아름 <yangareum0130@gmail.com>
2025-05-10 11:54:44 +0900

    docs : readme zustand 정리

---
+++ zustand/src/_compontntes/TodoInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useTodoStore } from "../store/todoStore";
+
+export default function TodoInput() {
+  const [inputValue, setInputValue] = useState("");
+  const { addTodo } = useTodoStore((state) => state.actions);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim()) return;
+    addTodo(inputValue);
+    setInputValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요.."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">추가</button>
+    </form>
+  );
+}
---
+++ zustand/src/store/filterStore.js
@@ -0,0 +1,1 @@
+// filter state = all(전체), completed(완료), active(미완료)
