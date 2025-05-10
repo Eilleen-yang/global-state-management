482c554c18664f5bf846639a4a8931f27bf023d5
양아름 <yangareum0130@gmail.com>
2025-05-10 13:46:09 +0900

    feat: filter기능 구현 +버튼추가

--- zustand/src/_compontntes/TodoInput.jsx
+++ zustand/src/_compontntes/TodoInput.jsx
@@ -2,24 +2,35 @@ import { useState } from "react";
 import { useTodoStore } from "../store/todoStore";
 
 export default function TodoInput() {
-  const [inputValue, setInputValue] = useState("");
-  const { addTodo } = useTodoStore((state) => state.actions);
+  // const [inputValue, setInputValue] = useState("");
+  const [text, setText] = useState("");
+  const addTodo = useTodoStore((state) => state.addTodo);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!inputValue.trim()) return;
-    addTodo(inputValue);
-    setInputValue("");
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (!inputValue.trim()) return;
+  //   addTodo(inputValue);
+  //   setInputValue("");
+  // };
+
+  const handleAdd = () => {
+    if (!text.trim()) return;
+
+    addTodo(text);
+    setText("");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <div
+    // onSubmit={handleSubmit}
+    >
       <input
         type="text"
         placeholder="할 일을 입력하세요.."
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        // value={inputValue}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
-      <button type="submit">추가</button>
-    </form>
+      <button onClick={handleAdd}>추가</button>
+    </div>
   );
 }
--- zustand/src/store/todoStore.js
+++ zustand/src/store/todoStore.js
@@ -1,33 +1,41 @@
 import { create } from "zustand";
 
-async function fetchTodosFormAPI() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/todos?_limit=5"
-  );
-  if (!response.ok) throw new Error("할 일 목록 가져오기 실패");
+// async function fetchTodosFormAPI() {
+//   const response = await fetch(
+//     "https://jsonplaceholder.typicode.com/todos?_limit=5"
+//   );
+//   if (!response.ok) throw new Error("할 일 목록 가져오기 실패");
 
-  const data = await response.json();
-  return data;
-}
+//   const data = await response.json();
+//   return data;
+// }
+
+let id = 1;
 
 export const useTodoStore = create((set) => ({
   todos: [],
-  isLoading: false,
-  error: null,
-  actions: {
-    fetchTodos: async () => {
-      set({ isLoading: true, error: null });
-      try {
-        const todos = await fetchTodosFormAPI();
-        set({ todos, isLoading: false }); // todos 단축 문법
-      } catch (error) {
-        set({ error: error.message, isLoading: false });
-      }
-    },
-  },
+  // isLoading: false,
+  // error: null,
+  // actions: {
+  //   fetchTodos: async () => {
+  //     set({ isLoading: true, error: null });
+  //     try {
+  //       const todos = await fetchTodosFormAPI();
+  //       set({ todos, isLoading: false }); // todos 단축 문법
+  //     } catch (error) {
+  //       set({ error: error.message, isLoading: false });
+  //     }
+  //   },
+  // },
   addTodo: (title) =>
     set((state) => ({
-      todos: [...state.todos, { id: Date.now(), title }],
+      todos: [...state.todos, { id: id++, title, completed: false }],
+    })),
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
     })),
   removeTodo: (id) =>
     set((state) => ({
