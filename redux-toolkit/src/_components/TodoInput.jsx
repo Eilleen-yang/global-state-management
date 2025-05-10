a93208c2c56bc83c4d112696a166586d5adedf4d
양아름 <yangareum0130@gmail.com>
2025-05-10 14:33:12 +0900

    feat: 약간의 코드수정, README.md작성

--- redux-toolkit/src/_components/TodoInput.jsx
+++ redux-toolkit/src/_components/TodoInput.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 export default function TodoInput() {
   const [text, setText] = useState("");
   const dispatch = useDispatch();
+
   const handleAdd = () => {
     if (text.trim() === "") return;
     dispatch(addTodo(text));
@@ -16,6 +17,7 @@ export default function TodoInput() {
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        placeholder="할일을 입력해주세요."
       />
       <button onClick={handleAdd}>추가</button>
     </div>
